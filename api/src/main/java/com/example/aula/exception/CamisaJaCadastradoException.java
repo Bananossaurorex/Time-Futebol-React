a27948ba9bdbc9119580ab964f67e2bbdeef913e
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2025-05-20 20:31:55 -0300

    Delete api directory

--- api/src/main/java/com/example/aula/exception/CamisaJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class CamisaJaCadastradoException extends RuntimeException {
-    public CamisaJaCadastradoException(String message) {
-        super(message);
-    }
-}
